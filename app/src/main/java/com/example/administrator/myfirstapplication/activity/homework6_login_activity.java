8b9cd0b00fb9e7a1d62b91b2235746febfb4609a
ChenKuiHan <1182663770@qq.com>
2016-05-26 18:35:12 +0800

    20160526

--- app/src/main/java/com/example/administrator/myfirstapplication/activity/homework6_login_activity.java
+++ app/src/main/java/com/example/administrator/myfirstapplication/activity/homework6_login_activity.java
@@ -17,33 +17,7 @@ public class homework6_login_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar= new Toolbar(this);
-//        setContentView(R.layout.baseactivity);
-//        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("My App");
-        setSupportActionBar(toolbar);
-        toolbar.setBackgroundColor(0xff999999);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.normal_btn:
-                        AlertDialog.Builder b=new AlertDialog.Builder(homework6_login_activity.this);
-                        b.setTitle("about");
-                        b.setIcon(R.drawable.icon_11);
-                        b.setMessage("create by ChenKuiHan");
-                        b.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                //Toast.makeText(BaseActivity.this, "", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        b.create().show();
-                        break;
-                }
-                return true;
-            }
-        });
+
         setContentView(R.layout.login);
     }
 }
